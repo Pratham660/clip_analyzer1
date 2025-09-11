99833ba93d74b61d531f3b010bb1349e6aa9b681
Pratham660 <prathamagarwal660@gmail.com>
2025-09-11 10:26:53 +0100

    minor chg

--- pages/2_OI_Analysis.py
+++ pages/2_OI_Analysis.py
@@ -29,7 +29,7 @@ df.replace([np.inf, -np.inf], np.nan, inplace=True)
 tuple_cols = ["OI_Close_Corr", "Close_Pctls", "OI_Pctls", "Vol_Pctls"]
 for col in tuple_cols:
     df[col] = df[col].apply(safe_eval)
-st.set_page_config(page_title="OI Dashboard", layout="wide")
+st.set_page_config(page_title="OI DashB", layout="wide")
 
 with st.sidebar:
     st.header("Filters")
@@ -74,77 +74,77 @@ st.dataframe(filtered_df, use_container_width=True)
 st.text("Note : The tick size taken is 0.005")
 st.text('Note : The cols "OI_Close_Corr", "Close_Pctls", "OI_Pctls", "Vol_Pctls" are based on last 20 Period calculations of 1D, 5D, 10D, 20D')
 
-'''
-import pandas as pd
-import numpy as np
-close_df = pd.read_pickle("I:/SYB_PHM/Proj_1/logs/global_close_dict_multithread.pkl")
-global_volume_df = pd.read_pickle("I:/SYB_PHM/Proj_1/logs/global_volume_dict_multithread.pkl")
+# '''
+# import pandas as pd
+# import numpy as np
+# close_df = pd.read_pickle("I:/SYB_PHM/Proj_1/logs/global_close_dict_multithread.pkl")
+# global_volume_df = pd.read_pickle("I:/SYB_PHM/Proj_1/logs/global_volume_dict_multithread.pkl")
 
-def volume_shocker_analysis():
-    for k in [k for k in global_volume_df if len(global_volume_df[k])==0]:
-        del global_volume_df[k]
+# def volume_shocker_analysis():
+#     for k in [k for k in global_volume_df if len(global_volume_df[k])==0]:
+#         del global_volume_df[k]
 
-    strategies_list_vol = list( set(k[0] for k in global_volume_df) )
-    time_in_mins_list = [60, 120, 240,1440] 
-    rows = []
+#     strategies_list_vol = list( set(k[0] for k in global_volume_df) )
+#     time_in_mins_list = [60, 120, 240,1440] 
+#     rows = []
 
-    for strategy in strategies_list_vol:
-        for time_in_mins in time_in_mins_list:
-            key = (strategy, str(time_in_mins)+'min')
-            df = global_volume_df.get(key)
+#     for strategy in strategies_list_vol:
+#         for time_in_mins in time_in_mins_list:
+#             key = (strategy, str(time_in_mins)+'min')
+#             df = global_volume_df.get(key)
 
-            if df is None or df.empty:
-                # global_volume_df[key] = pd.DataFrame()
-                continue
-            else:
-                for col in df.columns:
-                    series = df[col]
-                    idx = 0 if series.index[0] > series.index[-1] else -1
-                    latest_time = series.index[idx].strftime('%H:%M:%S')
+#             if df is None or df.empty:
+#                 # global_volume_df[key] = pd.DataFrame()
+#                 continue
+#             else:
+#                 for col in df.columns:
+#                     series = df[col]
+#                     idx = 0 if series.index[0] > series.index[-1] else -1
+#                     latest_time = series.index[idx].strftime('%H:%M:%S')
                     
-                    df1 = series[ series.index.strftime('%H:%M:%S') == latest_time ]
-                    idx = 0 if df1.index[0] > df1.index[-1] else -1
-                    if df1.empty or df1.isna().any():
-                        continue
-                    mul=1
-                    if idx == 0:
-                        mul=-1
-                    else:
-                        avg_5period_volume = df1.iloc[idx- mul*6 : idx].mean()
-                        avg_10period_volume = df1.iloc[idx- mul*11 : idx].mean()
-                        avg_20period_volume = df1.iloc[idx- mul*21 : idx].mean()
-                        rows.append(
-                            [
-                                strategy,
-                                time_in_mins,
-                                key,
-                                col,
-                                df1.iloc[idx],
-                                idx,
-                                df1.index[idx] ,
-                                avg_5period_volume,
-                                avg_10period_volume,
-                                avg_20period_volume,
-                                df1.iloc[idx] / avg_5period_volume if avg_5period_volume != 0 else np.nan,
-                                df1.iloc[idx] / avg_10period_volume if avg_10period_volume != 0 else np.nan,
-                                df1.iloc[idx] / avg_20period_volume if avg_20period_volume != 0 else np.nan,
-                            ])
+#                     df1 = series[ series.index.strftime('%H:%M:%S') == latest_time ]
+#                     idx = 0 if df1.index[0] > df1.index[-1] else -1
+#                     if df1.empty or df1.isna().any():
+#                         continue
+#                     mul=1
+#                     if idx == 0:
+#                         mul=-1
+#                     else:
+#                         avg_5period_volume = df1.iloc[idx- mul*6 : idx].mean()
+#                         avg_10period_volume = df1.iloc[idx- mul*11 : idx].mean()
+#                         avg_20period_volume = df1.iloc[idx- mul*21 : idx].mean()
+#                         rows.append(
+#                             [
+#                                 strategy,
+#                                 time_in_mins,
+#                                 key,
+#                                 col,
+#                                 df1.iloc[idx],
+#                                 idx,
+#                                 df1.index[idx] ,
+#                                 avg_5period_volume,
+#                                 avg_10period_volume,
+#                                 avg_20period_volume,
+#                                 df1.iloc[idx] / avg_5period_volume if avg_5period_volume != 0 else np.nan,
+#                                 df1.iloc[idx] / avg_10period_volume if avg_10period_volume != 0 else np.nan,
+#                                 df1.iloc[idx] / avg_20period_volume if avg_20period_volume != 0 else np.nan,
+#                             ])
                             
-    df = pd.DataFrame(rows, columns=['strategy', 'time_in_mins', 'key','product', 'volume', 'idx', 'series_idx',
-                                                    'avg_5period_volume', 'avg_10period_volume', 'avg_20period_volume','5pd_multiple', '10pd_multiple',  '20pd_multiple'])
-    return df
-df = volume_shocker_analysis()
-df = df.loc[ (df["5pd_multiple"]>1) & (df["volume"]>999) ,["strategy" ,"time_in_mins" ,"product" ,"volume", 
-                                                           "5pd_multiple","10pd_multiple","20pd_multiple",] ]
-## MACD + RSI SIGNAL
-resampled_df = pd.read_pickle("I:/SYB_PHM/Proj_1/logs/resampled_data_dict_multithread.pkl")
-df = resampled_df['SRA_3MS', 'SRAZ25-H26', '60min'].copy()
-import ta
-df['rsi_14'] = ta.momentum.rsi(df['close'], window=14, fillna=True)
-macd_oject = ta.trend.MACD(df['close'], window_slow=26, window_fast=12, window_sign=9, fillna=True)
-df['macd_diff'] = macd_oject.macd_diff()
-df['macd'] = macd_oject.macd()
-df['macd_signal'] = macd_oject.macd_signal()
-df['rsi_percentiles'] = df['rsi_14'].rank(pct=True) * 100
-df['macd_diff_percentiles'] = df['macd_diff'].rank(pct=True) * 100
-'''
+#     df = pd.DataFrame(rows, columns=['strategy', 'time_in_mins', 'key','product', 'volume', 'idx', 'series_idx',
+#                                                     'avg_5period_volume', 'avg_10period_volume', 'avg_20period_volume','5pd_multiple', '10pd_multiple',  '20pd_multiple'])
+#     return df
+# df = volume_shocker_analysis()
+# df = df.loc[ (df["5pd_multiple"]>1) & (df["volume"]>999) ,["strategy" ,"time_in_mins" ,"product" ,"volume", 
+#                                                            "5pd_multiple","10pd_multiple","20pd_multiple",] ]
+# ## MACD + RSI SIGNAL
+# resampled_df = pd.read_pickle("I:/SYB_PHM/Proj_1/logs/resampled_data_dict_multithread.pkl")
+# df = resampled_df['SRA_3MS', 'SRAZ25-H26', '60min'].copy()
+# import ta
+# df['rsi_14'] = ta.momentum.rsi(df['close'], window=14, fillna=True)
+# macd_oject = ta.trend.MACD(df['close'], window_slow=26, window_fast=12, window_sign=9, fillna=True)
+# df['macd_diff'] = macd_oject.macd_diff()
+# df['macd'] = macd_oject.macd()
+# df['macd_signal'] = macd_oject.macd_signal()
+# df['rsi_percentiles'] = df['rsi_14'].rank(pct=True) * 100
+# df['macd_diff_percentiles'] = df['macd_diff'].rank(pct=True) * 100
+# '''
